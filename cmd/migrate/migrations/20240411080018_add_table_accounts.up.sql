fe0a1c42a9c83f429049ef40ee3d6816a364657b
petrostrak <pit.trak@gmail.com>
2024-04-12 11:42:52 +0300

    FIX typo in add_table_accounts_up.sql

--- cmd/migrate/migrations/20240411080018_add_table_accounts.up.sql
+++ cmd/migrate/migrations/20240411080018_add_table_accounts.up.sql
@@ -2,5 +2,5 @@ create table if not exists accounts(
     id serial primary key,
     user_id uuid references auth.users,
     username text not null,
-    created_at timestampz not null default now()
+    created_at timestamp not null default now()
 );
